57664333cf6e27d43014a8a665df281d647628a4
adriangmzretamal <99414251+adriangmzretamal@users.noreply.github.com>
2026-02-03 15:10:18 +0100

    Add files via upload

--- p1/Libro.java
+++ p1/Libro.java
@@ -1,3 +1,9 @@
+/**
+ * Esta clase contiene la información de un libro así como su comportamiento en una biblioteca
+ * @author: Adrián Gómez y Javier Agüero. adrian.gomezretamal@estudiante.uam.es y javier.aguero@estudiante.uam.es
+ * @version: 1.0
+ * Nombre del fichero: Libro.java
+ */
 public class Libro {
     private String isbn;
     private String titulo;
@@ -6,13 +12,31 @@ public class Libro {
     private String genero;
     private int anyoPublicacion;
 
-        public Libro(String isbn, String titulo, String autor, int ejemplaresDisponobles) {
+    /**
+     * Constructor de la clase Libro
+     * Contiene la información de Libro
+     * @param isbn String del isbn de un libro
+     * @param titulo String del titulo de un libro
+     * @param autor String del autor de un libro
+     * @param ejemplaresDisponibles int del número de ejemplares disponibles de un libro
+     */
+    public Libro(String isbn, String titulo, String autor, int ejemplaresDisponobles) {
         this.isbn = isbn;
         this.titulo = titulo;
         this.autor = autor;
         this.ejemplaresDisponibles = ejemplaresDisponobles;
     }
 
+    /**
+     * Constructor de la clase Libro
+     * Contiene la información de Libro con las nuevas modificaciones
+     * @param isbn String del isbn de un libro
+     * @param titulo String del titulo de un libro
+     * @param autor String del autor de un libro
+     * @param ejemplaresDisponibles int del número de ejemplares disponibles de un libro
+     * @param generi String del genero de un libro
+     * @param anyoPublicacion int del año de publicación de un libro
+     */
     public Libro(String isbn, String titulo, String autor, int ejemplaresDisponobles, String genero, int anyoPublicacion) {
         this.isbn = isbn;
         this.titulo = titulo;
@@ -22,12 +46,16 @@ public class Libro {
         this.anyoPublicacion = anyoPublicacion;
     }
 
-    // Método para verificar si el libro está disponible
+    /**
+     * Método para verificar si el libro está disponible
+     */
     public boolean estaDisponible() {
         return this.ejemplaresDisponibles > 0;
     }
 
-    // Método para prestar el libro
+    /**
+     * Método para prestar el libro
+     */
     public boolean prestar() {
         if (estaDisponible()) {
             this.ejemplaresDisponibles--;
@@ -36,25 +64,38 @@ public class Libro {
         return false;
     }
 
-    // Método para devolver el libro
+    /**
+     * Método para devolver el libro
+     */
     public void devolver() {
         this.ejemplaresDisponibles++;
     }
 
-    // Método para obtener la descripción del libro
+    /**
+     * Método para obtener la descripción del libro
+     */
     private String descripcion() {
         String estado = this.estaDisponible() ? "Disponible" : "No disponible";
         return "'"+this.titulo + "' de " + this.autor + " [" + estado + "[";
     }
 
+    /**
+     * Método para obtener el género del libro
+     */
     public String getGenero() {
         return genero;
     }
 
+    /**
+     * Método para obtener el año de publicación del libro
+     */
     public int getAnyoPublicacion() {
         return anyoPublicacion;
     }
 
+    /**
+     * Método para imprimir la información del libro
+     */
     @Override
     public String toString(){
         String ret = "ISBN: ";
