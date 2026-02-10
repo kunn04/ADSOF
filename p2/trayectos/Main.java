11f18b3a2a006fd82105fecc9f10493c833c0f16
Javier Agüero <javieraguero104@gmail.com>
2026-02-10 15:30:04 +0100

    main para probar el apartado 1

--- p2/trayectos/Main.java
+++ p2/trayectos/Main.java
@@ -3,8 +3,13 @@ package trayectos;
 public class Main{
     public static void main(String[] args){
         TramoTrayecto[] trayecto = {
-            new TramoTren("Sol Renfe, "Cantoblanco Renfe", Linea.C4, 4),
+            new TramoAPie("Hotel Puerta del Sol", "Sol Renfe", 1),
+            new TramoTren("Sol Renfe", "Cantoblanco Renfe", Linea.C4, 4),
+            new TramoApie("Cantoblanco Renfe", "EPS", 2.6, Ritmo.RAPIDO),
 
-        }
+        };
+
+        for (TramoTrayecto tramo: trayecto)
+            System.out.println(tramo);
     }
 }
